09f0c2ee6b183d3d759b339d7b8c768998475a66
SRISHANTH P M <srishanth232007@gmail.com>
2025-08-11 16:06:29 +0530

    26/60 solved.

---
+++ 26.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int multiplication(int number1, int number2){
+    return number1 * number2;
+}
+
+int main(){
+    int number1 = 10;
+    int number2 = 5;
+
+    int result = multiplication(number1, number2);
+
+    printf("The multipplication of two numbers is %d", result);
+}

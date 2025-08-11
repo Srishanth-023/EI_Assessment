4dac14f7948cf14187c25d062d16ee592b05a3cf
SRISHANTH P M <srishanth232007@gmail.com>
2025-08-11 05:43:56 +0530

    10/60 solved.

---
+++ 10.c
@@ -0,0 +1,9 @@
+int main(){
+    // Declaring and Initializing basic data types.
+    int age = 18;
+    char grade = 'O';
+    char name[] = "Srishanth";
+    float cgpa = 9.48;
+
+    return 0;
+}

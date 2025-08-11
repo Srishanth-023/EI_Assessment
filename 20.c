a2190447c7a759c1169fa12b0437ee47c2681ead
SRISHANTH P M <srishanth232007@gmail.com>
2025-08-11 14:54:09 +0530

    20/60 solved.

---
+++ 20.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int number1 = 99;
+    int number2 = 1;
+
+    int addedResult = number1 + number2;
+
+    printf("The sum of two numbers is %d", addedResult);
+}

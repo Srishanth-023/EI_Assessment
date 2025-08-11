f2d729d70fbe9d48f65de2ff60a3c17e84418368
SRISHANTH P M <srishanth232007@gmail.com>
2025-08-11 16:08:12 +0530

    26/60 solved.

--- 26.c
+++ 26.c
@@ -5,7 +5,7 @@ int multiplication(int number1, int number2){
 }
 
 int main(){
-    int number1 = 10;
+    int number1 = 4;
     int number2 = 5;
 
     int result = multiplication(number1, number2);

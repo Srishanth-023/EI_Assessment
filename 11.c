9f8b64672af044027bfc6f23ae947fd84d4a2202
SRISHANTH P M <srishanth232007@gmail.com>
2025-08-11 14:30:56 +0530

    11/60 solved.

--- 11.c
+++ 11.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
 
-int sum(int number1, int number2){
-    return number1 + number2;
-}
-
 int main(){
-    int number1 = 2;
-    int number2 = 3;
-
-    int result = sum(number1, number2);
-
-    printf("The sum of 2 and 3 is %d", result);
+    printf("The sum of 2 and 3 is %d", 2 + 3);
 }
